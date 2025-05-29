c39a800393415de112706ca58d7291d6ab5b229c
jfstrainer <rajendra.sp@teknoturf.com>
2025-05-29 14:01:44 +0530

    Update NameComparator.java

--- NameComparator.java
+++ NameComparator.java
@@ -6,7 +6,7 @@ public class NameComparator implements Comparator<Employee> {
 
 	@Override
 	public int compare(Employee emp1, Employee emp2) {
-		return emp1.compareTo(emp2);
+		return emp1.getName().compareTo(emp2.getName());
 	}
 
 }
